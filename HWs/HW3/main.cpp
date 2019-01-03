53610b67965611b1f56463cfaf4d88d2d0c348b9
temurtas <halil.temurtas@metu.edu.tr>
2019-01-03 22:12:22 +0300

    update 2019_01_03

---
+++ HWs/HW3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "treenode.h"
+#include "bstree.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    BinSTree<int> tree;
+    return 0;
+}
